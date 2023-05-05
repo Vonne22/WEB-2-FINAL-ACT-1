1105053db64505f26171cbada0038e5198bfa7ab
Vonne22 <126044931+Vonne22@users.noreply.github.com>
2023-05-05 20:27:58 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7s9OlqxSDpljh7KN_nQ6YFIWLMUaxe8g",
+  authDomain: "fir-page-8da3f.firebaseapp.com",
+  projectId: "fir-page-8da3f",
+  storageBucket: "fir-page-8da3f.appspot.com",
+  messagingSenderId: "196988643117",
+  appId: "1:196988643117:web:7fe979290498ca2a31d5a4"
+};
+
+  firebaseConfig.initailizeApp(firebaseConfig);
+
+  import index from './WEB 2 FINAL ACT 1/index'
+    export default function home(){
+        return {
+           
+        }
+    }
